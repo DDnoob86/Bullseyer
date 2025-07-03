9a7c1895c63da4009ebe7f151bf0b0cf98ff930a
DDnoob86 <daniel.hubert86@web.de>
2025-07-03 20:06:47 +0200

    Delete bullseyer_full directory

--- bullseyer_full/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'tailwindcss';
-export default defineConfig({
-  content: ['./src/**/*.{html,svelte,js,ts}'],
-  theme: { extend: { colors: { primary: '#1d4ed8' } } },
-  plugins: []
-});
